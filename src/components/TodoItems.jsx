3a416d180c3a6d5352d0fa8e1dbd3d51fd6ba9d8
tutorials-git <agarwalhitesh551@gmail.com>
2024-06-23 16:33:00 +0530

    second change

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -5,21 +5,21 @@ import deleteIcon from '../assets/delete.png';
 
 const TodoItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
   return (
-    <div className='flex items-center my-6 gap-2'>
+    <div className='flex items-center my-3 gap-2'>
       <div className='flex flex-1 items-center cursor-pointer'>
         <img
           onClick={() => toggle(id)}
-          className='w-14'
+          className='w-7'
           src={isComplete ? tick : notTick}
           alt={isComplete ? 'Completed' : 'Not Completed'}
         />
-        <p className={`text-slate-700 ml-14 text-7xl ${isComplete ? 'line-through' : ''}`}>
+        <p className={`text-slate-700 ml-4 text-[17px] ${isComplete ? 'line-through' : ''}`}>
           {text}
         </p>
       </div>
       <img
         onClick={() => deleteTodo(id)}
-        className='w-16 mr-24 cursor-pointer'
+        className='w-4 cursor-pointer'
         src={deleteIcon}
         alt='Delete'
       />
